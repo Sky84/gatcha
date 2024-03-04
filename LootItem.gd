6bea7ca2f4e92a26cd5ceb4c0e5a8e1683576f66
Etienne SC <etienne.salupocorvo@gmail.com>
2024-03-04 22:11:18 +0100

    feat: animating loot

--- LootItem.gd
+++ LootItem.gd
@@ -13,7 +13,5 @@ var item_id: String = '';
 var item_rarity: ITEM_RARITY;
 @onready var texture_rect = $TextureRect;
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	var creature = Creatures.Creatures.get(item_id);
-	texture_rect.texture = load(creature.visuals[item_rarity]);
+func init_item(item_data):
+	texture_rect.texture = load(item_data.visuals[item_data.type].baby);
--- LootPanel.gd
+++ LootPanel.gd
@@ -3,10 +3,58 @@ class_name LootPanel
 
 const LOOT_ITEM = preload("res://LootBoxes/loot_item.tscn");
 
-func open(shop_item: ShopItem):
-	var shop_item_data = shop_item.get_object_data();
-	$LootBox/TextureRect.texture = load(shop_item_data.visual);
-	show();
+var opened_loot_item: Array = [];
 
-func _on_loot_box_pressed():
-	pass # Replace with function body.
+@onready var start_loot_position: Vector2 = $StartLootPosition.global_position;
+
+func open(shop_item: ShopItem):
+	show();
+	var shop_item_data = shop_item.get_object_data();
+	_process_loot_rate(shop_item_data);
+
+func _process_loot_rate(shop_item_data):
+	if opened_loot_item.size() < 9:
+		var loot_type: String = get_lootbox_type(shop_item_data);
+		var creature = get_random_creature().duplicate(true);
+		var instance_loot: LootItem = LOOT_ITEM.instantiate();
+		add_child(instance_loot);
+		instance_loot.position = start_loot_position;
+		creature.type = loot_type;
+		instance_loot.init_item(creature);
+		update_loot_node_position(instance_loot);
+		opened_loot_item.append(instance_loot);
+	
+func update_loot_node_position(instance_loot: TextureButton):
+	var margin = 100; 
+	var target_pos_x = ((opened_loot_item.size() % 3) * 210) + margin;
+	var target_pos_y = ((opened_loot_item.size() / 3) * 210) + margin;
+	var target_pos = Vector2(target_pos_x, target_pos_y);
+	var tween = get_tree().create_tween();
+	tween.tween_property(instance_loot, "position", target_pos, 0.5).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT);
+
+func get_lootbox_type(shop_item_data) -> String:
+	var total_rate = shop_item_data["wood_loot_rate"] + shop_item_data["bronze_loot_rate"] + shop_item_data["silver_loot_rate"] + shop_item_data["gold_loot_rate"] + shop_item_data["diamond_loot_rate"]
+	var random_number = randf() * total_rate;
+	var current_rate = 0;
+
+	var possible_rewards = [];
+
+	possible_rewards.append("wood");
+	if random_number < (current_rate + shop_item_data["bronze_loot_rate"]):
+		possible_rewards.append("bronze");
+	if random_number < (current_rate + shop_item_data["silver_loot_rate"]):
+		possible_rewards.append("silver");
+	if random_number < (current_rate + shop_item_data["gold_loot_rate"]):
+		possible_rewards.append("gold");
+	if random_number < (current_rate + shop_item_data["diamond_loot_rate"]):
+		possible_rewards.append("diamond");
+
+	
+	var chosen_reward = possible_rewards[randi() % possible_rewards.size()];
+	print("Received ", chosen_reward, " from the ", shop_item_data["name"], " lootbox!")
+	return chosen_reward;
+
+func get_random_creature() -> Dictionary:
+	var random_creature_id = Creatures.Creatures.keys()[randi() % Creatures.Creatures.size()]; # Sélection aléatoire d'un ID de créature parmi les disponibles
+	var creature = Creatures.Creatures[random_creature_id];
+	return creature;
